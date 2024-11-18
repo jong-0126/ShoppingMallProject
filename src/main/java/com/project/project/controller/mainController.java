4227914b9c1a6a851adc872d57317e44c0fd24b5
Harher-kim <mj0330w@gmail.com>
2024-11-18 23:34:37 +0900

    header 코드 수정

--- src/main/java/com/project/project/controller/mainController.java
+++ src/main/java/com/project/project/controller/mainController.java
@@ -2,6 +2,7 @@ package com.project.project.controller;
 
 import com.project.project.domain.entity.Item;
 import com.project.project.repository.ItemRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,10 +21,21 @@ public class mainController {
 
 
     @GetMapping("/main")
-    public String mainView(Model model){
+    public String mainView(Model model, HttpSession session){
         // 랜덤으로 상품 가져오기
         List<Item> randomItems = itemRepository.findRandomItems();
         model.addAttribute("items", randomItems);
+
+        Boolean isSuperAdmin = (Boolean) session.getAttribute("isSuperAdmin");
+
+        if(isSuperAdmin != null && isSuperAdmin){
+            model.addAttribute("isAdmin", true);
+        }else{
+            model.addAttribute("isAdmin", false);
+        }
+
         return "main";
     }
+
+
 }
